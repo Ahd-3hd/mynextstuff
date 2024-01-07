28a9f61cb51ce78bf2ef0ce71d99a854bf0dddca
Ahd-3hd <ahdhani0@gmail.com>
2024-01-07 20:48:58 +0300

    localhost to 127

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 export default async function Home() {
-  const resp = await fetch("http://localhost:3000/api/hello");
+  const resp = await fetch("http://127.0.0.1:3000/api/hello");
 
   console.log(resp.status);
 
